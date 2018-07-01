6075e0a9a529c92e542d237fcbb0c6385ccd6031
ErickErraez <edg.erraez@yavirac.edu.ec>
2018-07-01 18:47:19 -0500

    Ubicacion

--- museosLeaflet/src/pages/map/map.ts
+++ museosLeaflet/src/pages/map/map.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import * as Leaflet from 'leaflet';
+import 'leaflet-draw';
 
 declare const L: any;
 
@@ -10,7 +11,6 @@ declare const L: any;
   templateUrl: 'map.html'
 })
 export class MapPage {
-
   map: any;
   data: any;
   estado: any;
@@ -24,17 +24,33 @@ export class MapPage {
   }
 
   drawMap(): void {
-    this.map = Leaflet.map('map').setView([-0.1836298, -78.4821206], 19);
+    this.map = Leaflet.map('map').setView([-0.1836298, -78.4821206], 18);
     Leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: 'AppTuto',
-      maxZoom: 19
+      maxZoom: 18
     }).addTo(this.map);
 
+
+    var map = this.map;
+
+    //web location
+    map.locate({ setView: true });
+
+    //when we have a location draw a marker
+    function onLocationFound(e) {
+
+      Leaflet.marker(e.latlng).addTo(map)
+        .bindPopup('Estas Aqui').openPopup();
+    }
+    map.on('locationfound', onLocationFound);
 
     //alert on location error
     function onLocationError(e) {
       alert(e.message);
     }
+
+    this.map.on('locationerror', onLocationError);
   }
+
 
 }
